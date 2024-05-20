d0823f60e09e551cd87cd9f0a3481dc5183379ab
Developer <114158508+snakedev24@users.noreply.github.com>
2024-05-20 10:35:27 +0530

    Changed the model to instruct version.

--- python/llm.py
+++ python/llm.py
@@ -9,7 +9,7 @@ os.environ["HF_TOKEN"] = "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"
 
 
 def load_pipeline():
-    model_id = "meta-llama/Meta-Llama-3-8B"
+    model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
     pipeline = transformers.pipeline(
     "text-generation",
@@ -19,9 +19,36 @@ def load_pipeline():
     )
     return pipeline
 
-def get_result(prompt):
+def get_result(user_prompt):
     pipeline = load_pipeline()
-    return pipeline(prompt)
+
+    messages = [
+    {"role": "system", "content": "You are a chatbot who always responds with a answer or greeting. You are talking to a user who is asking you a question."},
+    {"role": "user", "content": f"{user_prompt}"},
+    ]
+
+    prompt = pipeline.tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+
+    outputs = pipeline(
+    prompt,
+    max_new_tokens=256,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+    )
+
+    
+    return outputs[0]["generated_text"][len(prompt):]
 
 
 # ~/.cache/huggingface/
